c98e454d2f270db2df210940c5993c552f556e13
prasanth <prasanth.varikallu@yahoo.in>
2017-12-09 23:02:12 -0500

    Add 2nd coroutine to calculate minimum of given numbers

--- coroutines.py
+++ coroutines.py
@@ -8,7 +8,24 @@ def my_coroutine():
         print("Received ", val)
 
 
-it = my_coroutine()
-next(it)  # initialize it
-it.send(5)
-it.send('a')
+def call_1st_coroutine():
+    it = my_coroutine()
+    next(it)  # initialize it
+    it.send(5)
+    it.send('a')
+
+
+def minimum_finding_coroutine():
+    val = yield
+    while True:
+        val2 = yield val
+        val = min(val, val2)
+
+def call_2nd_coroutine():
+    it = minimum_finding_coroutine()
+    next(it)
+    print(it.send(5))
+    print(it.send(2))
+    print(it.send(8))
+
+call_2nd_coroutine()
