a2ae56e1d19a4a2ce29438b02d84dc7eacc1ece7
prasanth <prasanth.varikallu@yahoo.in>
2017-12-09 22:48:40 -0500

    Code first coroutine

--- coroutines.py
+++ coroutines.py
@@ -1,1 +1,14 @@
-print(5)
+# This file contains the implementations of the examples from the
+# website http://www.informit.com/articles/article.aspx?p=2320938
+
+
+def my_coroutine():
+    while True:
+        val = yield
+        print("Received ", val)
+
+
+it = my_coroutine()
+next(it)  # initialize it
+it.send(5)
+it.send('a')
